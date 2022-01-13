5c7ed6da9ce39b19b5ed45b230916aa031da9bf0
Cliothalia <81527916+Cliothalia@users.noreply.github.com>
2022-01-13 15:08:26 +0100

    connections station objects ipv namen

--- loader.py
+++ loader.py
@@ -21,14 +21,25 @@ def file_loader():
         next(reader)
         for row in reader:
             for station in stations:
-                # adds connections to stations
-                if row[0] == station.name:
-                    station.add_connection(row[1], row[2])
-                if row[1] == station.name:
-                    station.add_connection(row[0], row[2])
+                for connection in stations:
+                    if row[0] == station.name:
+                        if row[1] == connection.name:
+                            station.add_connection(connection)
+                            station.add_distance(connection, row[2])
+                    
+                    if row[1] == station.name:
+                        if row[0] == connection.name:
+                            station.add_connection(connection)
+                            station.add_distance(connection, row[2])
     
+
+    # test print statements (TO BE REMOVED)
+    for station in stations:
+        print(station.name)
+        for connection in station.connections:
+            print(connection.name, end=", ")
+        print()
+        print(station.distances)
+
+
     return stations
-
-
-if __name__ == "__main__":
-    file_loader()
