c915aac43b3c7c6353adb9ee40ab3fb1daa4895d
Cliothalia <81527916+Cliothalia@users.noreply.github.com>
2022-01-17 16:33:40 +0100

    code gebruikt nationale stations

--- loader.py
+++ loader.py
@@ -8,7 +8,7 @@ def file_loader():
     stations = []
 
 
-    with open("data/StationsHolland.csv") as file:
+    with open("data/StationsNationaal.csv") as file:
         reader = csv.reader(file)
         next(reader)
         for row in reader:
@@ -16,7 +16,7 @@ def file_loader():
             station = Station(row[0], row[1], row[2])
             stations.append(station)
 
-    with open("data/ConnectiesHolland.csv") as file:
+    with open("data/ConnectiesNationaal.csv") as file:
         reader = csv.reader(file)
         next(reader)
         for row in reader:
@@ -31,15 +31,5 @@ def file_loader():
                         if row[0] == connection.name:
                             station.add_connection(connection)
                             station.add_distance(connection, row[2])
-    
-
-    # test print statements (TO BE REMOVED)
-    for station in stations:
-        print(station.name)
-        for connection in station.connections:
-            print(connection.name, end=", ")
-        print()
-        print(station.distances)
-
-
+                            
     return stations
