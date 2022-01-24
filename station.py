47bd28742dab3841003a84852e5d2f9ee05581b6
Martino Marconi <72311628+Martino-Marconi@users.noreply.github.com>
2022-01-24 11:41:13 +0100

    Update station.py

--- station.py
+++ station.py
@@ -1,24 +1,28 @@
-
 class Station:
     def __init__(self, name, x_coord, y_coord):
         self.name = name
         self.x_coord = x_coord
         self.y_coord = y_coord
 
-        self.visited = False
-
-        self.connections = []
-        self.distances = {}
-
-    def add_connection(self, next_station):
-        self.connections.append(next_station)
-
-    def add_distance(self, next_station, distance):
-
-        self.distances[next_station] = float(distance)
+        self.connections = {}
+        self.used_connections = []
     
-    def get_connections(self):
-        return self.connections
+    def add_connection(self, connected_station, distance):
+        self.connections[connected_station] = distance
+    
+    def pick_shortest_connection(self, current_station):
 
-    def set_visited(self):
-        self.visited = True
+        distance_counter = 10000
+        next_station = ""
+        stations = current_station.connections
+
+        for station in stations:
+            if int(stations[station]) < distance_counter:
+                if station not in current_station.used_connections:
+                    distance_counter = stations[station]
+                    next_station = station
+
+        return next_station
+    
+    def get_travel_time(self, connection):
+        return self.connections[connection]
