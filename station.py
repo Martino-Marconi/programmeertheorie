617cf249fd0ea1853282c05e790708af453ef03b
Cliothalia <81527916+Cliothalia@users.noreply.github.com>
2022-01-31 22:50:23 +0100

    Update station.py

--- station.py
+++ station.py
@@ -1,3 +1,6 @@
+import random
+
+
 class Station:
     def __init__(self, name, x_coord, y_coord):
         self.name = name
@@ -6,27 +9,45 @@ class Station:
 
         self.connections = {}
         self.used_connections = []
-        self.visited = False
-    
+        self.visited = 0
+
     def add_connection(self, connected_station, distance):
+        """
+        Adds connected stations with distance to self.connections dict
+        """
+
         self.connections[connected_station] = distance
-    
+
     def pick_shortest_connection(self, current_station):
-        
+        """
+        Picks shortest possible connection out of connected stations
+        """
+
         distance_counter = 10000
         next_station = None
         stations = current_station.connections
+        possible_stations = []
 
         for station in stations:
+            possible_stations.append(station)
             if int(stations[station]) < distance_counter:
                 if station not in current_station.used_connections and current_station not in station.used_connections:
                     distance_counter = stations[station]
                     next_station = station
 
+        if next_station is None:
+            next_station = random.choice(possible_stations)
+
         return next_station
-    
+
     def get_travel_time(self, connection):
+        """
+        Returns the distance to a connection
+        """
         return self.connections[connection]
-    
+
     def set_visited(self):
-        self.visited = True
+        """
+        Set visited to True if a station has been visited already
+        """
+        self.visited += 1
