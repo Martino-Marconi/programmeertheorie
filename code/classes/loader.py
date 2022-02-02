7810bd9ccf8c61c44a98db207814d9f5b4bd0fc8
Cliothalia <81527916+Cliothalia@users.noreply.github.com>
2022-02-02 21:33:20 +0100

    clean version

--- code/classes/loader.py
+++ code/classes/loader.py
@@ -1,33 +1,39 @@
-import csv
-
 from code.classes.station import Station
 
-def file_loader():
+import csv
 
+
+def file_loader():
+    """
+    Load data from csv files and make station classes
+    based on that data.
+    """
     stations = []
 
+    # get station names and coordinates from csv
     with open("input/StationsNationaal.csv") as file:
         reader = csv.reader(file)
         next(reader)
+
         for row in reader:
-            # creates stations from csv file
+            # creates station classes
             station = Station(row[0], row[1], row[2])
             stations.append(station)
 
+    # get station connections from csv
     with open("input/ConnectiesNationaal.csv") as file:
         reader = csv.reader(file)
         next(reader)
         for row in reader:
             for station in stations:
                 for connection in stations:
+                    # add connections to station objects that match names in csv file
                     if row[0] == station.name:
                         if row[1] == connection.name:
                             station.add_connection(connection, float(row[2]))
-                    
+
                     if row[1] == station.name:
                         if row[0] == connection.name:
                             station.add_connection(connection, float(row[2]))
 
     return stations
-
-
