1d576c55ae3adfc25e8d4fe982858b33ca1554aa
Cliothalia <81527916+Cliothalia@users.noreply.github.com>
2022-02-02 21:30:06 +0100

    clean version main.py

--- main.py
+++ main.py
@@ -2,33 +2,34 @@ from code.algorithms import a_random, b_semi_random, shortest_connection
 from code.classes.parameters import Random, Random_whc, Semi_random, Semi_random_whc, Shortest_con, Shortest_con_whc
 from code.classes import parameters
 
-
+import argparse
 import time
-from sys import argv
 
 if __name__ == "__main__":
 
-    # 
+    # create a command line argument parser
+    parser = argparse.ArgumentParser(description="Build train network")
+    parser.add_argument("algorithm", type=str, help="Choose algorithm. Options: random, semi-random, shortest-connection")
+    parser.add_argument("runs", type=int, help="Number of runs")
+    parser.add_argument("trains", type=int, default=20, help="Number of trains")
+    parser.add_argument("duration", type=int, default=180, help="Maximum train duration")
+
+    # parse command line arguments
+    args = parser.parse_args()
+
+    # assign command line arguments to variables
+    # max_time in minutes; run_time in seconds
+    routes = args.trains
+    max_time = args.duration
+    run_time = args.runs
+
+    # create algorithm classes
     random = Random()
     random_whc = Random_whc()
     semi_random = Semi_random()
     semi_random_whc = Semi_random_whc()
     shortest_con = Shortest_con()
     shortest_con_whc = Shortest_con_whc()
-
-    # check command line arguments
-    if len(argv) not in [1,2, 3, 4, 5]:
-        print("Usage: python3 main.py [algorithm] [runs] [available trains] [train duration]")
-        exit(1)
-
-    # command line arguments
-    # nog afmaken
-
-    # allocate data to required values
-    # max time in minutes, run time in seconds
-    routes = 20
-    max_time = 180
-    run_time = 40
 
     # set timer and amount of runs to default
     start = time.time()
@@ -40,23 +41,21 @@ if __name__ == "__main__":
             if i == n_runs:
                 print(f"runs: {n_runs}")
                 break
-        
+
         # --------------------- RANDOM ---------------------
         # this algorithm randomly choses it stops
         # set hill climber True/False if run with(out) hill climber
         a_random.run(routes, max_time, random, random_whc, hill_climber=True)
 
-
         # ------------------ SEMI_RANDOM ------------------
         # this algorithm semi-randomly choses it stops
-        # it picks it stop randemly, but a already visited 
+        # it picks it stop randemly, but a already visited
         # station cannot be visited again
         # set hill climber True/False if run with(out) hill climber
         b_semi_random.run(routes, max_time, semi_random, semi_random_whc, hill_climber=True)
 
-
         # ---------------SHORTEST_CONNECTION --------------
-        # this algorithm choses it stops based on the 
+        # this algorithm choses it stops based on the
         # shortest connection
         # set hill climber True/False if run with(out) hill climber
         shortest_connection.run(routes, max_time, shortest_con, shortest_con_whc, hill_climber=True)
@@ -66,10 +65,3 @@ if __name__ == "__main__":
 
     # plot the results in a histogram
     parameters.plot_results(n_runs, [random, random_whc, semi_random, semi_random_whc, shortest_con, shortest_con_whc])
-
-
-
-
-
-
-
