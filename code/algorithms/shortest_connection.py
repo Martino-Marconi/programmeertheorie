3b64ede66b71cdfb3f6a2be23bbe916a5890bdb2
maxwesterman <90262207+maxwesterman@users.noreply.github.com>
2022-02-02 19:07:27 +0100

    Update shortest_connection.py

--- code/algorithms/shortest_connection.py
+++ code/algorithms/shortest_connection.py
@@ -24,16 +24,16 @@ def pick_first_stop(stations, stops, train):
     # return first_stop
 
 
-    # ----------- ONE CONNECTION ------------------
-    first_stop = pick_one_connection_station(stations, stops)
+#     # ----------- ONE CONNECTION ------------------
+#     first_stop = pick_one_connection_station(stations, stops)
     
+#     return first_stop
+
+
+    # ----------- SHORTEST CONENCTION -------------
+    first_stop = pick_shortest_connection_start(stations, stops, train)
+
     return first_stop
-
-
-    # # ----------- SHORTEST CONENCTION -------------
-    # first_stop = pick_shortest_connection_start(stations, stops, train)
-
-    # return first_stop
 
 
 
