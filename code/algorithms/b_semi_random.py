50a94b5c8454e9cc2ee690c6172bfda5bbab5761
Cliothalia <81527916+Cliothalia@users.noreply.github.com>
2022-02-02 21:31:20 +0100

    clean version

--- code/algorithms/b_semi_random.py
+++ code/algorithms/b_semi_random.py
@@ -3,25 +3,25 @@ from code.classes.train import Route
 import code.classes.score as score
 import code.algorithms.hill_climber as hc
 
-
-import copy
 import random
+
 
 def pick_first_stop(stations, stops):
     """
-    Pick the first stop for each train. Semi-random algorithm, 
-    because it is not possible to start at a station that is 
+    Pick the first stop for each train. Semi-random algorithm,
+    because it is not possible to start at a station that is
     already visited.
     """
     while True:
-        
         first_stop = random.choice(stations)
 
         if first_stop not in stops:
             break
+
         continue
 
     return first_stop
+
 
 def pick_random_connection(current_station, stops):
     """
@@ -33,59 +33,62 @@ def pick_random_connection(current_station, stops):
     for stations in current_station.connections:
         empty_list.append(stations)
 
-    
     for i in range(100):
         next_station = random.choice(empty_list)
-        
+
         if next_station not in stops:
             break
 
     return next_station
 
+
 def run(routes, max_time, semi_random, semi_random_whc, hill_climber):
     """
     Randomly assign each station with one of the possible connections.
     """
-    
+
     # import Train class as object called 'tr'
     tr = Train(routes, max_time)
     ro = Route(routes, max_time)
-    
 
     # loop through all trains
     while ro.train_counter <= routes:
         ro.add_trains(ro.train_counter)
         train = ro.trains[(ro.train_counter - 1)]
-        
-        # pick random first stop 
+
+        # pick random first stop
         tr.current_station = pick_first_stop(ro.stations, ro.all_stops)
         tr.current_station.set_visited()
         train.stops.append(tr.current_station)
         ro.all_stops.append(tr.current_station)
 
-        # keep adding stations to list as long as train doesnt reach it's max time        
+        # keep adding stations to list as long as train doesn't reach its max time
         while tr.time_travelled <= max_time:
-            
-            tr.next_station = pick_random_connection(tr.current_station, ro.all_stops)
 
+            tr.next_station = pick_random_connection(tr.current_station, ro.all_stops)
             distance = tr.current_station.get_travel_time(tr.next_station)
 
             # stop adding next stops if next stop will overwrite the maximum time
             if (distance + tr.time_travelled) > max_time:
                 break
 
-            if tr.next_station == None:
+            # stop if there are no available stations
+            if tr.next_station is None:
                 break
 
+            # stop if all connections have been used before
             if tr.no_useable_connections():
                 break
 
+            # stop if all stations have been visited
             if len(ro.all_stops) == 62:
                 break
 
+            # add travel time
             tr.add_travel_time(distance)
-            train.stops.append(tr.next_station)
 
+            # add station to train's visited stops
+            train.stops.append(tr.next_station)
             tr.next_station.set_visited()
             tr.use_connections(tr.current_station, tr.next_station)
             ro.all_stops.append(tr.current_station)
@@ -93,37 +96,25 @@ def run(routes, max_time, semi_random, semi_random_whc, hill_climber):
 
         tr.time_travelled = 0
         ro.train_counter += 1
-        
+
+        # break if all stations have been visited
         if ro.all_stations_visited(ro.stations):
             break
-        
 
-    
     # once number of required trains is reached, print + plot results and calculate score
     final_score = score.calculate_score(ro.trains, ro.stations)
     ro.get_train_data()
 
     semi_random.append_score(final_score)
     higher = semi_random.check_if_highest(final_score)
-    
-    if higher == True:
+
+    if higher is True:
         ro.plot(ro.train_data)
 
-        data_file = f"output/semi_random.csv"
+        data_file = "output/semi_random.csv"
         score.print_results(final_score, ro.train_data, data_file)
 
-
     # # ----------- HILL CLIMBER ------------------
-    if hill_climber == True:
-        data_file = f"output/semi_random_whc.csv"
+    if hill_climber is True:
+        data_file = "output/semi_random_whc.csv"
         hc.hill_climber_algorithm(ro, final_score, semi_random_whc, data_file)
-        
-
-    
-
-
-
-
-        
-
-
