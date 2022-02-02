1e93a1190ee5dbfa7a58725a86cf97d64f84a61d
Cliothalia <81527916+Cliothalia@users.noreply.github.com>
2022-02-02 21:31:45 +0100

    clean version

--- code/algorithms/hill_climber.py
+++ code/algorithms/hill_climber.py
@@ -2,14 +2,23 @@ import code.classes.score as score
 
 import copy
 
+
 def hill_climber_algorithm(ro, final_score, algorithm, data_file):
+    # deep copy current train data
     tmp_data = copy.deepcopy(ro.train_data)
     tmp_trains = copy.deepcopy(ro.trains)
     tmp_stations = copy.deepcopy(ro.stations)
-    ro.improve_route()
+
+    # improve route by adding stations to trains who have not travelled
+    # for long enough
+    ro.improve_train_length()
 
     for i in range(100):
-        ro.improve_route_2()
+        # improve route further by picking different end stations and seeing
+        # if they improve the score
+        ro.improve_last_stop()
+
+        # if score has improved, keep new route; otherwise not
         new_score = score.calculate_score(ro.trains, ro.stations)
         if new_score < final_score:
             ro.train_data = tmp_data
@@ -20,13 +29,17 @@ def hill_climber_algorithm(ro, final_score, algorithm, data_file):
             tmp_trains = copy.deepcopy(ro.trains)
             tmp_stations = copy.deepcopy(ro.stations)
             final_score = new_score
-    
-    ro.improve_trains_2()
+
+    # remove trains that go to stops that are all already visited by other trains
+    ro.improve_trains_superfluous()
+
+    # update train data
     ro.get_train_data()
 
+    # calculate and plot scores
     algorithm.append_score(final_score)
     higher = algorithm.check_if_highest(final_score)
-    
-    if higher == True:
+
+    if higher is True:
         ro.plot(ro.train_data)
         score.print_results(final_score, ro.train_data, data_file)
